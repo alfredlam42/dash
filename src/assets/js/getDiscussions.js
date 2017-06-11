b1dd76ae766fe4297366c2f778e3dfc604906122
alfredlam42 <alfred.lam.42@gmail.com>
2017-06-11 10:31:33 -0700

    updated discussions

---
+++ src/assets/js/getDiscussions.js
@@ -0,0 +1,21 @@
+function getResources(){
+  return [
+    {
+      id: 1,
+      title: 'Lorem Ipsum',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun'
+    },
+    {
+      id: 2,
+      title: 'Lorem Ipsum',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun'
+    },
+    {
+      id: 3,
+      title: 'Lorem Ipsum',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun'
+    },
+  ]
+}
+
+export default getResources;
--- src/components/community/discussions/Discussions.js
+++ src/components/community/discussions/Discussions.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
 // Components
+import Guide from '../guide/Guide';
 
 // CSS
 import './Discussions.css';
 
+// Helpers
+import getDiscussions from '../../../assets/js/getDiscussions';
+
 class Discussions extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+      guides: []
+    }
+  }
+
+  componentWillMount(){
+    var guides = getDiscussions();
+
+    this.setState({guides: guides});
+  }
+
+  renderDiscussions(){
+    var guides = this.state.guides;
+
+    return guides.map(function(guide){
+      return <Guide key={guide.id} guide={guide} />
+    })
+  }
+
   render(){
     return(
       <div className="discussions">
         <div className="discussions-wrapper">
-          This is the discussions page.
+          {this.renderDiscussions()}
         </div>
       </div>
     )
