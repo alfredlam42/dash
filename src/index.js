bc253916f847e7234742ba8027ca80272e75e587
alfredlam42 <alfred.lam.42@gmail.com>
2017-06-11 03:51:52 -0700

    updated routes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import useScroll from 'react-router-scroll/lib/useScroll';
 
 // Components
 import App from './App';
-import Home from './home/Home';
+import Home from './components/home/Home';
+import SchoolList from './components/schoolList/SchoolList';
+import ReviewPage from './components/reviewPage/ReviewPage';
 
 // Helpers
 import registerServiceWorker from './registerServiceWorker';
@@ -17,6 +19,8 @@ ReactDOM.render(
   <Router history={browserHistory} render={applyRouterMiddleware(useScroll())}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="/schools" component={SchoolList} />
+      <Route path="/schools/review" component={ReviewPage} />
     </Route>
   </Router>,
   document.getElementById('root')
