89fb7a164797fc5091201eb3039bffcaf7e5eaca
alfredlam42 <alfred.lam.42@gmail.com>
2017-06-11 04:46:14 -0700

    added images and updated nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -8,15 +8,33 @@ import './Nav.css';
 
 // Helpers
 
+// Images
+var school = require('../../assets/img/school.png');
+var review = require('../../assets/img/review.png');
+var report = require('../../assets/img/report.png');
+var settings = require('../../assets/img/settings.png');
+
 class Nav extends Component {
   render(){
     return(
       <div className="navigation">
         <div className="navigation-wrapper">
-          <div className="link"><FontAwesome name="home" /></div>
-          <div className="link"><FontAwesome name="star" /></div>
-          <div className="link"><FontAwesome name="files-o" /></div>
-          <div className="link"><FontAwesome name="cog" /></div>
+          <div className="link">
+            <img src={school} alt="schools icon" />
+            School
+          </div>
+          <div className="link">
+            <img src={review} alt="review icon" />
+            Review
+          </div>
+          <div className="link">
+            <img src={report} alt="reports icon" />
+            Report
+          </div>
+          <div className="link">
+            <img src={settings} alt="setting icon" />
+            Setting
+          </div>
         </div>
       </div>
     )
