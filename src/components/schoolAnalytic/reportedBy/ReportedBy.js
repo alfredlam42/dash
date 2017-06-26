93e328bbdfb833c6490a9ef580af8e2f301b76d6
alfredlam42 <alfred.lam.42@gmail.com>
2017-06-25 17:53:19 -0700

    added new charts to work for school analysis

--- src/components/schoolAnalytic/reportedBy/ReportedBy.js
+++ src/components/schoolAnalytic/reportedBy/ReportedBy.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 // Components
+import { Pie } from 'react-chartjs-2';
 
 // CSS
 import './ReportedBy.css';
@@ -9,9 +10,41 @@ import './ReportedBy.css';
 
 class ReportedBy extends Component {
   render(){
+    var femaleNumber = Math.floor(Math.random() * 100);
+    var maleNumber = Math.floor(Math.random() * 100);
+    var parentNumber = Math.floor(Math.random() * 100);
+
+    var data = {
+      datasets: [{
+        data: [femaleNumber, maleNumber, parentNumber],
+        backgroundColor:[
+          'rgba(146, 242, 235, 1)',
+          'rgba(245, 59, 46, 1)',
+          'rgba(255, 193, 7, 1)'
+        ]
+      }],
+      labels: [
+        'Female Students',
+        'Male Students',
+        'Parents'
+      ],
+    };
+
+    var options = {
+      maintainAspectRatios: true,
+      legend: {
+        display: true,
+        position: 'right',
+        labels: {
+          boxWidth: 14
+        }
+      }
+    }
+
     return(
       <div className="reported-by">
-        ReportedBy
+        <div className="reported-by-title">Issues Reported By</div>
+        <Pie data={data} height="160" options={options} />
       </div>
     )
   }
--- src/components/schoolAnalytic/reportedCases/ReportedCases.js
+++ src/components/schoolAnalytic/reportedCases/ReportedCases.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 // Components
+import { HorizontalBar } from 'react-chartjs-2';
 
 // CSS
 import './ReportedCases.css';
@@ -9,9 +10,53 @@ import './ReportedCases.css';
 
 class ReportedCases extends Component {
   render(){
+    var bullyingNumber = Math.floor(Math.random() * 100);
+    var sexualHarassmentNumber = Math.floor(Math.random() * 100);
+    var sanitationNumbers = Math.floor(Math.random() * 100);
+    var classNumber = Math.floor(Math.random() * 100);
+    var languageNumber = Math.floor(Math.random() * 100);
+    var enrollmentNumber = Math.floor(Math.random() * 100);
+
+    var data = {
+      datasets: [
+        {
+          data: [bullyingNumber, sexualHarassmentNumber, sanitationNumbers, classNumber, languageNumber, enrollmentNumber],
+          backgroundColor: [
+            'rgba(0, 200, 83, 1)',
+            'rgba(126, 23, 127, 1)',
+            'rgba(146, 242, 235, 1)',
+            'rgba(245, 59, 46, 1)',
+            'rgba(255, 193, 7, 1)',
+            'rgba(255, 143, 225, 1)',
+          ]
+        }
+      ],
+      labels: ['Bullying', 'Sexual Harassment', 'Sanitation Facilities', 'Class Size', 'Language Barrier', 'Enrollment'],
+    };
+
+    var options = {
+      maintainAspectRatios: true,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+          }
+        }]
+      }
+    }
+
     return(
       <div className="reported-cases">
-        ReportedCases
+        <div className="reported-cases-title">Number of Reported Cases</div>
+        <HorizontalBar height="300" data={data} options={options} />
       </div>
     )
   }
